c35cd81b215b32e93c67e0834cafd8fae165e394
DukeBas <duke.bas@gmail.com>
2022-01-04 21:54:11 +0100

    Miner resource scanning

--- src/Trainwreck/bots/Miner.java
+++ src/Trainwreck/bots/Miner.java
@@ -6,6 +6,10 @@ import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Miner extends Robot {
 
     public Miner(RobotController rc) {
@@ -22,14 +26,36 @@ public class Miner extends Robot {
         MapLocation myLocation = rc.getLocation();
 
         /*
-         * find all resources in range.
+         * find all resources in range, start by getting all map locations in sight.
          */
-        // get all map locations in sight
-        MapLocation[] nearbyLocations = rc.getAllLocationsWithinRadiusSquared(myLocation, 1000); // uses 1000 instead of vision range as it will only go up to robot vision range anyways
-        // Filter out locations without resources, add the number of resources in a combined object.
+        MapLocation[] nearbyLocations = rc.getAllLocationsWithinRadiusSquared(myLocation, this.visionRadiusSquared);
 
+        /*
+         * Filter out locations without resources, add the number of resources in a combined object.
+         * List sorted first on gold then lead.
+         */
+        List<LocationWithResources> ResourceLocations = new ArrayList<>();
+        for (MapLocation loc : nearbyLocations) {
+            int lead = rc.senseLead(loc);
+            int gold = rc.senseGold(loc);
+            if (lead > 0 || gold > 0) {
+                ResourceLocations.add(new LocationWithResources(loc, lead, gold));
+            }
+        }
+        Collections.sort(ResourceLocations);
 
-        //TODO filter resources to mining range (2 r^2)
+        /*
+         * Get the resources that are immediately mineable to us.
+         * Note: could be optimized by not creating a new array, filtering ResourceLocations and rearranging code.
+         */
+        List<LocationWithResources> MineableLocations = new ArrayList<>();
+        for (LocationWithResources lwr : ResourceLocations) {
+            if (myLocation.distanceSquaredTo(lwr.loc) <= this.actionRadiusSquared) {
+                MineableLocations.add(lwr);
+            }
+        }
+        // Depending on list implementation it should still be sorted, but to be sure.
+        Collections.sort(MineableLocations); // TODO find out if this really is unnecessary
 
         //TODO mine gold around us first
 
@@ -68,7 +94,7 @@ public class Miner extends Robot {
 /**
  * Record type of MapLocation and the amount of lead and gold on that position.
  */
-class LocationWithResources {
+class LocationWithResources implements Comparable<LocationWithResources> {
     public MapLocation loc;
     public int lead;
     public int gold;
@@ -77,5 +103,24 @@ class LocationWithResources {
         this.loc = loc;
         this.lead = lead;
         this.gold = gold;
+    }
+
+    /**
+     * First compare based on gold, then on lead.
+     *
+     * @param o LocationWithResources to compare to.
+     * @return positive if this is greater, negative if other is greater, 0 if they have equal values.
+     */
+    @Override
+    public int compareTo(LocationWithResources o) {
+        // first compare based on gold
+        int goldCompare = Integer.compare(this.gold, o.gold);
+        if (goldCompare != 0) {
+            return goldCompare;
+        } else {
+            // equal gold, compare on lead now
+            // both lead and gold the same.
+            return Integer.compare(this.lead, o.lead);
+        }
     }
 }
