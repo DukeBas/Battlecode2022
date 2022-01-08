f68f9d7cc4f09b8aac9972e703c42d316b932d4a
DukeBas <35384117+DukeBas@users.noreply.github.com>
2022-01-08 13:43:05 +0100

    isDroid function

--- src/Trainwreck/util/Helper.java
+++ src/Trainwreck/util/Helper.java
@@ -4,11 +4,25 @@ import battlecode.common.RobotType;
 
 public class Helper {
     /**
-     * Returns whether a robot type is considered a combat unit (Sage, Soldier, Watchtower)
+     * Returns whether a robot type is considered a combat unit (Sage, Soldier, Watchtower).
+     *
+     * @param type RobotType of the unit
      */
     public static boolean isCombatUnit(RobotType type) {
         return type == RobotType.SAGE
                 || type == RobotType.SOLDIER
                 || type == RobotType.WATCHTOWER;
+    }
+
+    /**
+     * Returns whether a robot type is considered a droid.
+     *
+     * @param type RobotType of the unit.
+     */
+    public static boolean isDroid(RobotType type) {
+        return type == RobotType.MINER
+                || type == RobotType.SAGE
+                || type == RobotType.SOLDIER
+                || type == RobotType.BUILDER;
     }
 }
