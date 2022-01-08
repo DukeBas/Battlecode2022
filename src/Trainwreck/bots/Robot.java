051efa4cb87867b70dd9c575d6d3c0c489eac244
DukeBas <35384117+DukeBas@users.noreply.github.com>
2022-01-08 16:19:38 +0100

    Robot builtByID

--- src/Trainwreck/bots/Robot.java
+++ src/Trainwreck/bots/Robot.java
@@ -7,13 +7,15 @@ import battlecode.common.*;
 import java.util.Objects;
 import java.util.Random;
 
+import static Trainwreck.util.Helper.isDroid;
+
 public abstract class Robot {
 
     /**
      * This is the RobotController singleton. You use it to perform actions from this robot,
      * and to get information on its current status.
      */
-    final RobotController rc;
+    protected final RobotController rc;
 
     /**
      * We will use this variable to count the number of turns this robot has been alive.
@@ -33,17 +35,34 @@ public abstract class Robot {
     /**
      * Interface to interact with the shared memory
      */
-    final Communication comms;
+    protected final Communication comms;
 
     /**
      * Variable to hold a location as a target enemy archon.
      */
-    MapLocation targetArchonLocation = null;
+    protected MapLocation targetArchonLocation = null;
 
     /**
      * Variable to store ID of enemy team
      */
-    final Team enemy;
+    protected final Team enemy;
+
+    /**
+     * Variable to store ID of own team
+     */
+    protected final Team friendly;
+
+    /**
+     * Variable to store own robot type, so compiler can (hopefully) optimize.
+     */
+    protected final RobotType ownType;
+
+    /**
+     * Variable to hold the ID of the archon/builder who originally built this robot.
+     * -1 for archons, as they are only generated at the start of the game.
+     * -2 if creator was not found. Could only happen if creator dies (Moves out of range?) before initialisation.
+     */
+    protected final int builtByID;
 
     /**
      * A random number generator.
@@ -55,17 +74,61 @@ public abstract class Robot {
 
     public Robot(RobotController rc) {
         this.rc = rc;
-        System.out.println("Hello world! I am a " + this.rc.getType());
+        this.ownType = rc.getType();
+        friendly = rc.getTeam();
+        enemy = friendly.opponent();
+
+        /*
+         * Get the ID of the original robot that built this robot.
+         */
+        int creatorID = -2; // default, signalling failure to find creator. We do this because builtByID is final.
+        if (ownType == RobotType.ARCHON) {
+            // Archons are not built by other robots!
+            creatorID = -1;
+        } else {
+            // Not an archon!
+            if (isDroid(ownType)) {
+                // Droid, always built by an archon!
+                RobotInfo[] nearbyFriendlies = rc.senseNearbyRobots(2, friendly);
+
+                /*
+                 * Go through nearby friendly units until an archon is found.
+                 * even if this is not the exact correct one, it means that the archons are very close
+                 * together, so it should not matter in the grand scheme of things.
+                 */
+                for (RobotInfo bot : nearbyFriendlies) {
+                    if (bot.getType() == RobotType.ARCHON) {
+                        creatorID = bot.getID();
+                        break;
+                    }
+                }
+            } else {
+                // Not a droid, so must be either a laboratory or watchtower.
+                // NOTE: WE ASSUME HERE BUILD RANGE IS 5 (ACTION RADIUS), BUT IT COULD BE 2.
+                // Droid, always built by an archon!
+                RobotInfo[] nearbyFriendlies = rc.senseNearbyRobots(5, friendly);
+
+                /*
+                 * Go through nearby friendly units until a builder is found.
+                 * There might be multiple close, so this is currently not accurate.
+                 */
+                for (RobotInfo bot : nearbyFriendlies) {
+                    if (bot.getType() == RobotType.BUILDER) {
+                        creatorID = bot.getID();
+                        break;
+                    }
+                }
+            }
+        }
+        builtByID = creatorID;
 
         /*
          * Initialise communications object
          */
         comms = new FirstCommunication(rc);
 
-        actionRadiusSquared = rc.getType().actionRadiusSquared; // Need to check how this works out for Lab as it has no action range
-        visionRadiusSquared = rc.getType().visionRadiusSquared;
-
-        enemy = rc.getTeam().opponent();
+        actionRadiusSquared = ownType.actionRadiusSquared; // Need to check how this works out for Lab as it has no action range
+        visionRadiusSquared = ownType.visionRadiusSquared;
     }
 
     /**
@@ -86,14 +149,14 @@ public abstract class Robot {
                 // Oh no! It looks like we did something illegal in the Battlecode world. You should
                 // handle GameActionExceptions judiciously, in case unexpected events occur in the game
                 // world. Remember, uncaught exceptions cause your robot to explode!
-                System.out.println(rc.getType() + " GameAction-Exception");
+                System.out.println(ownType + " GameAction-Exception");
                 e.printStackTrace();
 
                 rc.setIndicatorString("GameActionException: " + e.toString());
             } catch (Exception e) {
                 // Oh no! It looks like our code tried to do something bad. This isn't a
                 // GameActionException, so it's more likely to be a bug in our code.
-                System.out.println(rc.getType() + " Generic-Exception");
+                System.out.println(ownType + " Generic-Exception");
                 e.printStackTrace();
 
                 rc.setIndicatorString("Exception: " + e.toString());
@@ -118,16 +181,27 @@ public abstract class Robot {
          * If we have a target location and can see it, then the archon was added before in comms.checkForEnemyArchons();
          * So only clear location if in vision range
          */
-        if (targetArchonLocation != null && rc.canSenseLocation(targetArchonLocation)){
+        if (targetArchonLocation != null && rc.canSenseLocation(targetArchonLocation)) {
             // if location does not contain an enemy archon, invalidate it
             RobotInfo robotAtLocation = rc.senseRobotAtLocation(targetArchonLocation);
-            if (!(robotAtLocation != null  && robotAtLocation.team == enemy)){
+            if (!(robotAtLocation != null && robotAtLocation.team == enemy)) {
                 // nothing here! clear target location!
                 rc.setIndicatorString("NOTHING HERE, CLEARING");
                 comms.invalidateLocationEnemyArchon(targetArchonLocation);
                 targetArchonLocation = null;
             }
         }
+
+        /*
+         * Increase unit counters, so archons know how many units of each type are alive at all times.
+         */
+        if (isDroid(ownType)) {
+
+        }
+    }
+
+    private increaseUnitCounter() {
+        comms.increaseUnitCounter();
     }
 
     /**
