698aa763a4fbaf23216fe0c2a35f2467457e74ce
DukeBas <35384117+DukeBas@users.noreply.github.com>
2022-01-09 23:53:35 +0100

    Communication getArchonOrder

--- src/Trainwreck/bots/Archon.java
+++ src/Trainwreck/bots/Archon.java
@@ -75,7 +75,7 @@ public class Archon extends Robot {
         /*
          * Pick direction to build in, prefer lighter terrain
          */
-
+        rc.setIndicatorString(comms.getArchonOrder(rc.getID()) + "");
 
     }
 
