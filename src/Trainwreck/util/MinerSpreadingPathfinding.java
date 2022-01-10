dfd8bca8f9e0e2e7f8e11adc3e977a594a3c112c
DukeBas <35384117+DukeBas@users.noreply.github.com>
2022-01-10 02:08:05 +0100

    First iteration spreading out miners

---
+++ src/Trainwreck/util/MinerSpreadingPathfinding.java
@@ -0,0 +1,130 @@
+package Trainwreck.util;
+
+import battlecode.common.*;
+
+import static Trainwreck.util.Helper.isCombatUnit;
+
+/**
+ * Makes miners avoid enemy combat units if possible, spread out from other each other
+ * and prefer to stay away from edges if possible.
+ */
+public class MinerSpreadingPathfinding implements Pathfinding {
+    private static final int RANGE_TO_CONSIDER_FRIENDLIES = 10;
+
+    @Override
+    public Direction getDirection(MapLocation source, MapLocation target, RobotController rc) {
+        MapLocation myLoc = rc.getLocation();
+
+        /*
+         * Avoid enemies. To conserve bytecode, only run from the first enemy spotted.
+         * Most often enemies are coming from one general direction so running from the first,
+         *  should be good enough for most situations.
+         */
+        RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(RobotType.MINER.visionRadiusSquared, rc.getTeam().opponent());
+        RobotInfo enemyCombatant = null;
+        for (RobotInfo bot : nearbyEnemies) {
+            if (isCombatUnit(bot.type)) { // Enemy combat unit spotted!
+                enemyCombatant = bot;
+                break;
+            }
+        }
+
+        // are enemies nearby?
+        try {
+            if (enemyCombatant != null) {
+                /*
+                 * Enemies in sight! Run in the opposite direction, picking the lowest possible rubble.
+                 */
+                return bestOpposite(rc, myLoc.directionTo(enemyCombatant.location));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            rc.setIndicatorString(e.toString());
+        }
+
+        /*
+         * No enemies near! Let's spread out from friendly miners and try not to go to the edge.
+         */
+        RobotInfo[] nearbyFriendlies = rc.senseNearbyRobots(RANGE_TO_CONSIDER_FRIENDLIES, rc.getTeam());
+        RobotInfo closestMiner = null;
+        int distClosestMiner = Integer.MAX_VALUE;
+        for (RobotInfo bot : nearbyFriendlies) {
+            int dist = myLoc.distanceSquaredTo(bot.location);
+            if (dist < distClosestMiner) { // we found a new closest miner!
+                closestMiner = bot;
+                distClosestMiner = dist;
+            }
+        }
+
+        if (closestMiner != null) { // there is another miner close!
+            try {
+                // get opposite direction
+                Direction dir = bestOpposite(rc, myLoc.directionTo(closestMiner.location));
+
+                // check if moving to this position puts us close to the edge (2 closest tiles to edge)
+                MapLocation toMoveTo = myLoc.add(dir);
+                if (!rc.onTheMap(toMoveTo.translate(2, 2)) ||
+                        !rc.onTheMap(toMoveTo.translate(-2, 2)) ||
+                        !rc.onTheMap(toMoveTo.translate(2, -2)) ||
+                        !rc.onTheMap(toMoveTo.translate(-2, -2))) { // Location is close to the edge!
+                    // Go toward friendly unit so they hopefully give us more space!
+                    return bestOpposite(rc, dir);
+                } else {
+                    return dir;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                rc.setIndicatorString(e.toString());
+            }
+        }
+
+        /*
+         * No other friendly units in range, use other pathfinding technique
+         */
+        Pathfinding pathfinder = new RandomPreferLessRubblePathfinding();
+        return pathfinder.getDirection(myLoc, myLoc, rc);
+    }
+
+    /**
+     * Returns the best way to run away from target direction. Considers rubble.
+     *
+     * @param toTarget
+     * @return
+     */
+    private Direction bestOpposite(RobotController rc, Direction toTarget) throws GameActionException {
+
+        MapLocation myLoc = rc.getLocation();
+
+        Direction opposite = toTarget.opposite();
+        Direction left = opposite.rotateLeft();
+        Direction right = opposite.rotateRight();
+
+        // have direct opposite as default
+        Direction best = opposite;
+        int lowestRubble = rc.senseRubble(myLoc.add(opposite));
+
+        // is slightly to the left better?
+        MapLocation leftLoc = myLoc.add(left);
+        if (!rc.isLocationOccupied(leftLoc)) { // check if tile is free
+            int leftRubble = rc.senseRubble(leftLoc);
+            if (leftRubble < lowestRubble) {
+                // left is better!
+                lowestRubble = leftRubble;
+                best = left;
+            }
+        }
+
+        // is slightly to the right better?
+        MapLocation rightLoc = myLoc.add(right);
+        if (!rc.isLocationOccupied(rightLoc)) { // check if tile is free
+            int rightRubble = rc.senseRubble(rightLoc);
+            if (rightRubble < lowestRubble) {
+                // right is better!
+                // no need to assign rubble value, since this is the last option
+                best = right;
+            }
+        }
+
+        return best;
+    }
+}
