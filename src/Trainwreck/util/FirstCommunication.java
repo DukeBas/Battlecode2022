ba09907c9131c1424335902d7429e4355dd17824
DukeBas <duke.bas@gmail.com>
2022-01-20 21:32:19 +0100

    Prep in communications for best base location

--- src/Trainwreck/util/FirstCommunication.java
+++ src/Trainwreck/util/FirstCommunication.java
@@ -6,51 +6,7 @@ import java.util.ArrayList;
 
 
 /**
- * The following division of the shared memory is used
- * +────────────────────────────────────+────────────────────────────────────+────────────────────────────────────+────────────────────────────────────+
- * | Index 0                            | Index 1                            | Index 2                            | Index 3                            |
- * | Mapping?                           | Status booleans first 8 bits,      | Enemy counter                      | .                                  |
- * |                                    | last 8 bits for counting no.       |                                    |                                    |
- * |                                    | potential enemy archon locations   |                                    |                                    |
- * |                                    |                                    |                                    |                                    |
- * | Index 4                            | Index 5                            | Index 6                            | Index 7                            |
- * | EnemyArchon1 ID+Loc                | EnemyArchon2 ID+Loc                | EnemyArchon3 ID+Loc                | EnemyArchon4 ID+Loc                |
- * |                                    |                                    |                                    |                                    |
- * | Index 8                            | Index 9                            | Index 10                           | Index 11                           |
- * | FriendlyArchon1 ID+Loc             | FriendlyArchon2 ID+Loc             | FriendlyArchon3 ID+Loc             | FriendlyArchon4 ID+Loc             |
- * |                                    |                                    |                                    |                                    |
- * | Index 12                           | Index 13                           | Index 14                           | Index 15                           |
- * | Archon1 Counter (Miner+Sage)       | Archon2 Counter (Miner+Sage)       | Archon3  Counter (Miner+Sage)      | Archon4 Counter (Miner+Sage)       |
- * |                                    |                                    |                                    |                                    |
- * | Index 16                           | Index 17                           | Index 18                           | Index 19                           |
- * | Archon1 Counter (Soldier+Builder)  | Archon2 Counter (Soldier+Builder)  | Archon3 Counter (Soldier+Builder)  | Archon4 Counter (Soldier+Builder)  |
- * |                                    |                                    |                                    |                                    |
- * | 20                                 | 21                                 | 22                                 | 23                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 24                                 | 25                                 | 26                                 | 27                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 28                                 | 29                                 | 30                                 | 31                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 32                                 | 33                                 | 34                                 | 35                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 36                                 | 37                                 | 38                                 | 39                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 40                                 | 41                                 | 42                                 | 43                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 44                                 | 45                                 | 46                                 | 47                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 48                                 | 49                                 | 50                                 | 51                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 52                                 | 53                                 | 54                                 | 55                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 56                                 | 57                                 | 58                                 | 59                                 |
- * | ;                                  | ;                                  |                                    |                                    |
- * | 60                                 | 61                                 | 62                                 | 63                                 |
- * | ;                                  | ;                                  | ;                                  |                                    |
- * +────────────────────────────────────+────────────────────────────────────+────────────────────────────────────+────────────────────────────────────+
- * <p>
- * <p>
- * We use the first 6 bits for x, next 6 for y, and last 4 for extra information for encoding locations.
+ * We use the first 6 bits for x, next 6 for y, and last 4 bits storing for extra information with the locations.
  */
 public class FirstCommunication implements Communication {
     private final static int NUMBER_MAX_ARCHONS = 4;
@@ -59,7 +15,7 @@ public class FirstCommunication implements Communication {
     private final static int INDEX_START_FRIENDLY_ARCHON = 8; // 8,9,10,11
     private final static int INDEX_START_ENEMY_ARCHON = 4; // 4,5,6,7
     private final static int INDEX_START_COUNTERS = 12; // uses 2 per archon, so 12,13,14,15,16,17,18,19
-    private final static int INDEX_STATUS_BOOLS_ENEMY_COUNTER = 2; // 2
+    private final static int INDEX_BASE_SCORE_AND_POTENTIAL_ENEMY_COUNTER = 2; // 2, left 11 bits base score
     private final static int INDEX_START_POTENTIAL_ENEMY_ARRAY = 20; // 20 onwards
 
 
@@ -126,6 +82,19 @@ public class FirstCommunication implements Communication {
 
         // we did not find the requested archon ID
         return -1;
+    }
+
+    /**
+     * Minimum score is 0, max score is 2047
+     */
+    @Override
+    public void setBestBaseLocation(MapLocation loc, int score) throws GameActionException {
+
+    }
+
+    @Override
+    public LocationWithValue getBestBaseLocation() throws GameActionException {
+        return null;
     }
 
     @Override
@@ -558,18 +527,25 @@ public class FirstCommunication implements Communication {
         return closestPotentialArchon;
     }
 
+
+    // use right 5 bits
     private int getPotentialEnemyArchonCounter() throws GameActionException {
-        return rc.readSharedArray(INDEX_STATUS_BOOLS_ENEMY_COUNTER) & 0b0000000011111111;
+        return rc.readSharedArray(INDEX_BASE_SCORE_AND_POTENTIAL_ENEMY_COUNTER) & 0b0000000000011111;
     }
 
     private void increasePotentialEnemyArchonCounter() throws GameActionException {
-        rc.writeSharedArray(INDEX_STATUS_BOOLS_ENEMY_COUNTER,
-                getPotentialEnemyArchonCounter() + 1);
+        int oldVal = rc.readSharedArray(INDEX_BASE_SCORE_AND_POTENTIAL_ENEMY_COUNTER);
+        // At most realistically 20, most of the time ~12 at most? so we do not check. Hopefully this won't backfire
+        rc.writeSharedArray(INDEX_BASE_SCORE_AND_POTENTIAL_ENEMY_COUNTER,
+                oldVal + 1);
     }
 
     private void decreasePotentialEnemyArchonCounter() throws GameActionException {
-        rc.writeSharedArray(INDEX_STATUS_BOOLS_ENEMY_COUNTER,
-                getPotentialEnemyArchonCounter() - 1);
+        int oldVal = rc.readSharedArray(INDEX_BASE_SCORE_AND_POTENTIAL_ENEMY_COUNTER);
+        if ((oldVal & 0b0000000000011111) > 0) {
+            rc.writeSharedArray(INDEX_BASE_SCORE_AND_POTENTIAL_ENEMY_COUNTER,
+                    oldVal - 1);
+        }
     }
 
     @Override
